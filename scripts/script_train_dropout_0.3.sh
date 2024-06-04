5995673e0c5dc49f082d4f5c8901fd935882c6ef
tabeariepe <tabea.riepe@web.de>
2024-06-04 09:59:58 +0000

    added test script

---
+++ scripts/script_train_dropout_0.3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --qos=low
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=50G
+#SBATCH --time=5-00:00:00
+#SBATCH --container-mounts=/home/tabeariepe/Retina-SpliceAI/:/home
+#SBATCH --container-image="doduo1.umcn.nl#tabea/spliceai:2.3"
+#SBATCH --job-name=drop0.3
+#SBATCH --output=/home/tabeariepe/Retina-SpliceAI/slurm/dropout0.3.out
+
+cd /home/scripts/
+
+for i in {1..5}; 
+    do python3 train_model.py $i retina dropout train --dropoutrate 0.3 > ../output_train/SpliceAI_dropout0.3_retina${i}.txt;
+done
---
+++ scripts/test_model.py
@@ -0,0 +1,114 @@
+###############################################################################
+# This file contains code to test the SpliceAI model.
+###############################################################################
+
+import numpy as np
+import sys
+import time
+import h5py
+from keras.models import load_model
+from utils import *
+import argparse 
+from spliceai import categorical_crossentropy_2d
+
+# Read command line parameters 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model_name')
+parser.add_argument('test_dataset')
+
+# Parse the arguments
+args = parser.parse_args()
+
+model = args.model_name
+dataset = args.test_dataset
+
+###############################################################################
+# Load model and test data
+###############################################################################
+
+BATCH_SIZE = 6
+CL = 10000
+
+version = [1, 2, 3, 4, 5]
+
+model = [[] for v in range(len(version))]
+
+for v in range(len(version)):
+
+    model[v] = load_model('../models/' + model + str(version[v]) + '.h5', compile = False)
+                          
+    model[v].compile(loss=categorical_crossentropy_2d, optimizer='adam')
+    
+    print(model)
+      
+# Test on the retina data
+h5f = h5py.File('../data/' + dataset + '.h5', 'r')
+
+num_idx = len(list(h5f.keys()))//2
+
+print(dataset)
+
+###############################################################################
+# Model testing
+###############################################################################
+
+start_time = time.time()
+
+output_class_labels = ['Null', 'Acceptor', 'Donor']
+# The three neurons per output correspond to no splicing, splice acceptor (AG)
+# and splice donor (GT) respectively.
+
+for output_class in [1, 2]:
+
+    Y_true = [[] for t in range(1)]
+    Y_pred = [[] for t in range(1)]
+
+    for idx in range(num_idx):
+
+        X = h5f['X' + str(idx)][:]
+        Y = h5f['Y' + str(idx)][:]
+
+        Xc, Yc = clip_datapoints(X, Y, CL, 1)
+
+        Yps = [np.zeros(Yc[0].shape) for t in range(1)]
+
+        for v in range(len(version)):
+
+            Yp = model[v].predict(Xc, batch_size=BATCH_SIZE)
+
+            if not isinstance(Yp, list):
+                Yp = [Yp]
+
+            for t in range(1):
+                Yps[t] += Yp[t]/len(version)
+        # Ensemble averaging (mean of the ensemble predictions is used)
+
+        for t in range(1):
+
+            is_expr = (Yc[t].sum(axis=(1,2)) >= 1)
+
+            Y_true[t].extend(Yc[t][is_expr, :, output_class].flatten())
+            Y_pred[t].extend(Yps[t][is_expr, :, output_class].flatten())
+
+    print("\n%s:" % (output_class_labels[output_class]))
+
+    for t in range(1):
+
+        Y_true[t] = np.asarray(Y_true[t])
+        Y_pred[t] = np.asarray(Y_pred[t])
+
+        print_topl_statistics(Y_true[t], Y_pred[t])
+
+
+h5f.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print("--------------------------------------------------------------")
+
+###############################################################################
+
+
+
+
+
