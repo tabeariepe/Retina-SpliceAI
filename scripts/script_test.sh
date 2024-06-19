331ba0d955b6b67767d3f5c8c1dc035277b6243d
Tabea Riepe <tabea.riepe@radboudumc.nl>
2024-06-19 10:17:26 +0200

    added test script

---
+++ scripts/script_test.sh
@@ -0,0 +1,5 @@
+
+#GTEx
+python3 test_model.py ../models/SpliceAI_standard_gtex retina > outputs_test/SpliceAI_standard_gtex.txt
+python3 test_model.py ../models/SpliceAI_standard_gtex gtex >> outputs_test/SpliceAI_standard_gtex.txt
+python3 test_model.py ../models/SpliceAI_standard_gtex canonical >> outputs_test/SpliceAI_standard_gtex.txt
