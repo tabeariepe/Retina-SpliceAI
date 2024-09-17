7d06e22bb8d4a78e18e3662be99865322c2aedd7
Tabea Riepe <33683714+tabeariepe@users.noreply.github.com>
2024-09-17 11:57:32 +0200

    Update script_predictions_retina_control_exons.sh

--- validation_scripts/script_predictions_retina_control_exons.sh
+++ validation_scripts/script_predictions_retina_control_exons.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#SBATCH --qos=low
-#SBATCH --ntasks=1
-#SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=50G
-#SBATCH --time=5-00:00:00
-#SBATCH --container-mounts=/home/tabeariepe/Retina-SpliceAI-2/:/home
-#SBATCH --container-image="doduo1.umcn.nl#tabea/spliceai:2.3"
-#SBATCH --job-name=exons
-#SBATCH --exclude=dlc-electabuzz
-#SBATCH --output=/home/tabeariepe/Retina-SpliceAI-2/slurm/exons.out
 
 pip3 install pyfaidx
 
