19404ea6a4f100b867b0efdbb71478cfd74f1ced
Tabea Riepe <33683714+tabeariepe@users.noreply.github.com>
2024-09-17 11:11:07 +0200

    Update script_train_dropout_0.4.sh

--- scripts/script_train_dropout_0.4.sh
+++ scripts/script_train_dropout_0.4.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-#SBATCH --qos=low
-#SBATCH --ntasks=1
-#SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=50G
-#SBATCH --time=5-00:00:00
-#SBATCH --container-mounts=/home/tabeariepe/Retina-SpliceAI/:/home
-#SBATCH --container-image="doduo1.umcn.nl#tabea/spliceai:2.3"
-#SBATCH --job-name=drop0.4
-#SBATCH --output=/home/tabeariepe/Retina-SpliceAI/slurm/dropout0.4.out
-
-#cd /home/scripts/
 
 for i in {1..5}; 
     do python3 train_model.py $i retina dropout train --dropoutrate 0.4 > ../output_train_new/SpliceAI_dropout0.4_retina${i}.txt;
