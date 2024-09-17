e4d2fa8bbcd38c29ade26a736e9685c6b590d0b7
Tabea Riepe <33683714+tabeariepe@users.noreply.github.com>
2024-09-17 11:10:43 +0200

    Update script_train_dropout_0.2.sh

--- scripts/script_train_dropout_0.2.sh
+++ scripts/script_train_dropout_0.2.sh
@@ -14,4 +14,5 @@
 cd /home/scripts/
 
 for i in {1..5};
-  python3 train_model.py $i retina dropout train --dropoutrate 0.2 > ../output_train_new/SpliceAI_dropout0.2_new_retina${i}.txt;
+  do python3 train_model.py $i retina dropout train --dropoutrate 0.2 > ../output_train_new/SpliceAI_dropout0.2_new_retina${i}.txt;
+done
