5267995174b9c4c87ae41f10360b1246564a421c
Tabea Riepe <33683714+tabeariepe@users.noreply.github.com>
2024-09-17 11:12:45 +0200

    Update script_train_gtex.sh

--- scripts/script_train_gtex.sh
+++ scripts/script_train_gtex.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#SBATCH --qos=high
-#SBATCH --ntasks=1
-#SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=50G
-#SBATCH --time=5-00:00:00
-#SBATCH --container-mounts=/home/tabeariepe/Retina-SpliceAI/:/home
-#SBATCH --container-image="doduo1.umcn.nl#tabea/spliceai:2.3"
-#SBATCH --job-name=gtex
-#SBATCH --output=/home/tabeariepe/Retina-SpliceAI/slurm/gtex.out
-
-#cd /home/scripts/
-i=5
-python3 train_model.py $i gtex standard train > ../output_train_new/SpliceAI_standard_gtex${i}.txt;
-
+for i in {1..5};
+  do python3 train_model.py $i gtex standard train > ../output_train_new/SpliceAI_standard_gtex${i}.txt;
+done
